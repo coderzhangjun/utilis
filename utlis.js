93a2b148a209573bec8040a32fc608fcbdd57428
zhangjun <zj15305175214@163.com>
2023-03-10 21:43:33 +0800

    初步实现2返回2个数组的不同项

--- utlis.js
+++ utlis.js
@@ -1,3 +1,18 @@
-const sum = (a,b)=>{
-    return a +b
+// 返回2个数组的不同项
+const compareArr = ( arr1 ,arr2 )=>{
+    const diffArr = []
+    arr1.forEach(item1 => {
+        if(!arr2.includes(item1)){
+            diffArr.push(item1)
+        }
+    });
+    arr2.forEach(item2 => {
+        if(!arr1.includes(item2)){
+            diffArr.push(item2)
+        }
+    })
+    return diffArr
 }
+
+const newArr =  compareArr([6,10,11,19],[3,4,5,9])
+newArr.forEach(item=> console.log(item))
